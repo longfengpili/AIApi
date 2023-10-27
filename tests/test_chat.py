ff0742ab3149ac553fd46bd6ca70f938667cd627
longfengpili <398745129@qq.com>
2023-10-27 17:32:38 +0800

    [优化]test

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -2,11 +2,10 @@
 # @Author: longfengpili
 # @Date:   2023-09-20 14:05:00
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2023-09-20 14:11:44
+# @Last Modified time: 2023-10-27 17:31:59
 # @github: https://github.com/longfengpili
 
-
-from conf import APPID, APIKEY, APISECRET
+from xinghuo.config import AIConfig
 from xinghuo.chat import XinghuoChat
 from xinghuo.contents import Content, Contents
 
@@ -14,9 +13,10 @@ from xinghuo.contents import Content, Contents
 class TestChat:
 
     def setup_method(self, method):
+        aiconf = AIConfig.load('xinghuo')
         self.content = Content(**{'role': 'user', 'content': '介绍下pandas'})
         self.contents = Contents(self.content)
-        self.xhchat = XinghuoChat(APPID, APIKEY, APISECRET)
+        self.xhchat = XinghuoChat(aiconf.appname, aiconf.appid, aiconf.appkey, aiconf.appsecret)
 
     def teardown_method(self, method):
         pass
