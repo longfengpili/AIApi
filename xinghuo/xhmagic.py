f9ec76ce3da86be41ea679f07f4f69b14c8f6e60
longfengpili <398745129@qq.com>
2023-10-31 19:15:23 +0800

    [新增]aiconfig

--- xinghuo/xhmagic.py
+++ xinghuo/xhmagic.py
@@ -2,7 +2,7 @@
 # @Author: longfengpili
 # @Date:   2023-10-26 13:39:12
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2023-10-30 19:37:46
+# @Last Modified time: 2023-10-31 19:14:57
 # @github: https://github.com/longfengpili
 
 import re
@@ -25,20 +25,26 @@ from .config import AIConfig
 from .chat import XinghuoChat
 from .contents import Content, Contents
 
+from traitlets import Bool, Dict, Instance, Unicode, default, observe  # noqa
+from traitlets.config.loader import Config
+
 
 @magics_class
 class XhChater(Magics):
+    ainame = Unicode('ainame', help='ainame').tag(config=True)
 
     def __init__(self, shell: InteractiveShell = None):
         super(XhChater, self).__init__(shell)
+        self.ainame = 'xinghuo'
 
-    def xhchat(self, ainame: str = 'xinghuo'):
-        aiconf = AIConfig.load(ainame)
+    @property
+    def xhchat(self):
+        aiconf = AIConfig.load(self.ainame)
         if not aiconf:
             aiid = input('please input your appid:')
             aikey = input('please input your appikey:')
             aisecret = input('please input your appsecret:')
-            aiconf = AIConfig(ainame, aiid, aikey, aisecret)
+            aiconf = AIConfig(self.ainame, aiid, aikey, aisecret)
             aiconf.dump()
 
         xhchat = XinghuoChat(aiconf.appname, aiconf.appid, aiconf.appkey, aiconf.appsecret)
@@ -70,41 +76,92 @@ class XhChater(Magics):
 
         return '\n'.join([code for code in convert_codes if code])
 
-    @line_magic
-    def chat_single(self, line):
-        xhchat = self.xhchat()
-        contents = self.shell.user_ns.get('contents', Contents())
-        s_contents = contents[-2:]
-        sid, r_contents = xhchat.chat(line, s_contents, is_show_content=True)
-        # 传递给user_ns
+    def chat_jupyter(self, answer: str, number: int = 4, is_show_content: bool = True):
+        xhchat = self.xhchat
+        contents = self.shell.user_ns.get('CONTENTS', Contents())
+        s_contents = contents[-number:]
+        sid, r_contents = xhchat.chat(answer, s_contents, is_show_content=is_show_content)
+        # 新内容传递给user_ns
         contents.extend(r_contents[-2:])
-        self.shell.user_ns['contents'] = contents
+        self.shell.user_ns['CONTENTS'] = contents
+        return contents
 
+    def chat_input(self, contents):
         # 输出结果
-        # execution_id = self.shell.execution_count
-        # code = self._convert_to_code(contents.last_content)
-        # program_out = f"# Assistant Code for Cell [{execution_id}]:\n{code}"
-        # print('>>>>>>>>>>>>Result:\n')
-        # self.shell.run_cell(program_out)
+        execution_id = self.shell.execution_count
+        code = self._convert_to_code(contents.last_content)
+        program_out = f"# Assistant Code for Cell [{execution_id}]:\n{code}"
+        print('>>>>>>>>>>>>Result:\n')
+        self.shell.run_cell(program_out)
 
         # input to shell cell
-        # self.shell.set_next_input(program_out)
+        self.shell.set_next_input(program_out)
 
     @magic_arguments()
-    @argument('--number', '-n', default=5, type=int, help="how many contents to ai")
     @argument('--verobse', '-v', action="store_true", help="Whether to show ask")
     @argument('--save', '-s', action="store_true", help="Whether to save history")
-    @cell_magic
+    @argument('--input', action="store_true", help="Whether to save history")
+    @argument('--number', '-n', default=6, type=int, help="how many contents to ai")
+    @line_cell_magic
     def chat(self, line, cell=None):
+        '''[summary]
+        
+        [chat in jupyter]
+        
+        Args:
+            line ([str]): [line]
+            cell ([str]): [cell] (default: `None`)
+        '''
+        if not cell:
+            contents = self.chat_jupyter(line)
+            return
+
         args = parse_argstring(self.chat, line)
         is_show_content = True if args.verobse else False
+        number = args.number
+        contents = self.chat_jupyter(cell, number=number, is_show_content=is_show_content)
+
         is_save = True if args.save else False
-        contents = self._concat_contents('user', line)
-        print(f'>>>>>>Ask:{contents.last_content}\n')
-        xhchat = self.xhchat()
-        contents = xhchat.chat_stream(contents, is_show_content=is_show_content)
         if is_save:
-            contents.dump(xhchat.appname)
+            contents.dump(self.ainame)
+
+        isinput = args.input
+        if isinput:
+            self.chat_input(contents)
+
+    @line_magic
+    def aiconfig(self, line):
+        """Used for displaying and modifying xinghuo configurations.
+
+        Exemplar usage:
+
+        - %xhconfig
+          print all the configurable parameters and its current value
+
+        - %xhconfig <parameter_name>
+          print the current value of the parameter
+
+        - %xhconfig <parameter_name>=<value>
+          set the value of the parameter
+        """
+        line = line.strip().split('#')[0].strip()
+        all_class_configs = self.class_own_traits()
+
+        if not line or line.startswith('#'):
+            doc = self.class_get_help()
+            print(doc)
+            return
+        elif line in all_class_configs.keys():
+            return getattr(self, line)
+        elif '=' in line:
+            param, value = line.split('=')
+            param, value = param.strip(), value.strip()
+            if param in all_class_configs.keys():
+                cfg = Config()
+                exec(f'cfg.{self.__class__.__name__}.{line}', self.shell.user_ns, locals())
+                self.update_config(cfg)
+        else:
+            pass
 
 
 # 注册magic命令(如果使用mymagics.py, 则下边的函数不重要)
